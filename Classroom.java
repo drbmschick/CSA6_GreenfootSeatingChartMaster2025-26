f1075b510f5ec251e763f29410cbd10785f5ea78
BishalDas4321 <das8673@mydusd.org>
2025-09-03 14:26:45 -0700

    Add files via upload

--- Classroom.java
+++ Classroom.java
@@ -152,9 +152,9 @@ public class Classroom extends World
         KilgoreTrout kilgoretrout = new KilgoreTrout();
         addObject(kilgoretrout,2,3);
         kilgoretrout.assignSeat();
-        VinothYashwant vinothyashwant = new VinothYashwant();
-        addObject(vinothyashwant,5,6);
-        vinothyashwant.assignSeat();
+        BishalDas bishaldas = new BishalDas();
+        addObject(bishaldas,6,4);
+        bishaldas.assignSeat();
   
     }
     
@@ -229,5 +229,3 @@ public class Classroom extends World
    }
 }
 }
-
-
