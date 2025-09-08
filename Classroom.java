a83270236316f21256ca3d9eaf9e2bb03e76c408
krishayuth <154640442+Shay12345678@users.noreply.github.com>
2025-09-08 14:29:00 -0700

    Add files via upload

--- Classroom.java
+++ Classroom.java
@@ -152,6 +152,9 @@ public class Classroom extends World
         KilgoreTrout kilgoretrout = new KilgoreTrout();
         addObject(kilgoretrout,2,3);
         kilgoretrout.assignSeat();
+        UthayasegarKrishay uthayasegarkrishay = new UthayasegarKrishay();
+        addObject(uthayasegarkrishay,6,6);
+        uthayasegarkrishay.assignSeat();
   
     }
     
